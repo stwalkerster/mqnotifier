1a3cff6c2ac8f623ff7496d22545790e58c9662b
Simon Walker <github@stwalkerster.co.uk>
2022-02-26 01:51:06 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MqNotifier
+{
+    using System.IO;
+    using System.Text;
+    using RabbitMQ.Client;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = Environment.GetEnvironmentVariable("AMQP_HOSTNAME"),
+                Port = int.Parse(Environment.GetEnvironmentVariable("AMQP_PORT")),
+                VirtualHost = Environment.GetEnvironmentVariable("AMQP_HOSTNAME"),
+                UserName = Environment.GetEnvironmentVariable("AMQP_USERNAME"),
+                Password = Environment.GetEnvironmentVariable("AMQP_PASSWORD"),
+                ClientProvidedName = "MqNotifier",
+            };
+
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            var basicProperties = channel.CreateBasicProperties();
+            basicProperties.Type = Environment.GetEnvironmentVariable("AMQP_TYPE");
+            basicProperties.AppId = Environment.GetEnvironmentVariable("AMQP_APPID");
+            basicProperties.UserId = Environment.GetEnvironmentVariable("AMQP_USERNAME");
+            
+            var streamReader = new StreamReader(Console.OpenStandardInput());
+
+            channel.BasicPublish(
+                Environment.GetEnvironmentVariable("AMQP_EXCHANGE"),
+                string.Empty,
+                basicProperties,
+                Encoding.UTF8.GetBytes(streamReader.ReadLine()));
+            channel.Close();
+            connection.Close();
+        }
+    }
+}
